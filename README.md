31e91026a97282f840d85c1cb14f81b24c44495c
Toru KAWAMURA <tkawa@4bit.net>
2012-06-11 17:34:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Snooker Club
+=======
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snookerclub::Application.config.secret_token = 'fc3120d65bf4e1b585383c6e3cb2af6ea327677819350a5514297a8563ca1ad01349d1a253c1323bf0b03cd6b6b6ce0ed487a6d61754c67ae54374fd69a67315'
